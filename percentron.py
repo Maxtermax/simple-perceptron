7a8c50192728cfeb94c1e9496360c3ac63ed7a1c
maxtermax <esnene02@gmail.com>
2017-03-29 18:45:30 -0500

    pull last changes

--- percentron.py
+++ percentron.py
@@ -15,12 +15,21 @@ conjunto_de_entrenamiento = np.array([
 	[1, 1, 1]
 ])#Tabla compuesta NAND
 
+print "Conjunto de entramiento: %s "%(conjunto_de_entrenamiento)
+print "-"*60
+
 salidas_esperadas = np.array([
 	[1],
 	[1],
 	[1],
 	[0]
 ])#Resultado esperados de la tabla NAND
+
+print "Resultados esperados: %s"%(salidas_esperadas)
+
+print "-"*60
+print "Pesos inciales del perceptron: %s"%(pesos_iniciales)
+
 
 def activacion(sum, umbral):
 	if(sum > umbral): return 1 
@@ -35,6 +44,9 @@ def ajustar_pesos(entradas, pesos, ajuste):
 	return pesos#Retorna los pesos actualizados 
 
 def entrenamiento(entradas, salidas, pesos):
+	print "-"*60
+	print "Comienza entranamiento de la neurona."
+
 	for entreno in range(0, 10000):
 		#Entranar el perceptron 10000 veces
 		for i in range(0, len(entradas)):
@@ -42,7 +54,7 @@ def entrenamiento(entradas, salidas, pesos):
 			sum = np.dot(entradas[i], pesos)#Sumatoria de entradas y pesos
 			parcial = activacion(sum, umbral)#Resultado parcial
 			error = calcular_error(salidas[i], parcial)#Error cometido, (Aprendizaje supervisado)
-			if(entreno%1000 == 0): print "Error: %s"%(np.average(np.abs(error)))#Promedio del error cometido
+			if(entreno%1000 == 0): print "Promedio del error cometido: %s"%(np.average(np.abs(error)))#Promedio del error cometido
 			ajuste = tasa_de_aprendizaje*error#Ajustar el error en base a la tasa de aprendizaje 
 			pesos = ajustar_pesos(entradas[i], pesos, ajuste[0])#Actualiza los nuevos valores de los pesos ajustados
 	return pesos#retona los pesos ajustados
@@ -52,7 +64,8 @@ def prueba(entrada, pesos):
 	print "Para este caso: %s  la respuesta deberia ser: %s"%(entrada, activacion(sum, umbral))   
 
 pesos_entrenados = entrenamiento(conjunto_de_entrenamiento, salidas_esperadas ,pesos_iniciales)
+print "-"*60
 prueba([1, 1, 1], pesos_entrenados)
 
 
-#https://es.wikipedia.org/wiki/Perceptr%C3%B3n#Ejemplo
+#Fuentes: https://es.wikipedia.org/wiki/Perceptr%C3%B3n#Ejemplo
